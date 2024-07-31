d5f4e91a4f6ec424ab5a844a32473c0b5f9d96c9
Manu Gonzalez <manueljgonzalezg1698@gmail.com>
2024-07-31 12:18:17 +0200

    Cambio en fuente de app

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,12 +2,15 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
+		fontFamily: {
+			atomic: ["Atomic", "cursive"],
+		},
 		color:{
-			redESP: '#da392b',
-			black0f: '#0f0f0f',
-			black19: '#19191c',
-			whitef9: '#f9f9f9',
-			whitef7: '#f7f7f7',
+			'redESP': '#da392b',
+			'black0f': '#0f0f0f',
+			'black19': '#19191c',
+			'whitef9': '#f9f9f9',
+			'whitef7': '#f7f7f7',
 		},
 		extend: {},
 	},
