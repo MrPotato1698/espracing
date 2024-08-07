b56dde2aa24f2497023a2ca07287eabf92702bab
Manu Gonzalez <manueljgonzalezg1698@gmail.com>
2024-08-07 19:05:05 +0200

    chore: Update Astro config and package.json for server output and add Vercel integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,15 @@
-import { defineConfig } from "astro/config";
 import db from "@astrojs/db";
 import tailwind from "@astrojs/tailwind";
-
+import vercel from "@astrojs/vercel/serverless";
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), db()],
-  output: "hybrid"
+  output: "server",
+  adapter: vercel({
+    webAnalytics:{
+      enabled: true,
+    }
+  })
 });
