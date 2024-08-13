455f1ac6051544a5258b12c666814d9529d5265a
Manu Gonzalez <manueljgonzalezg1698@gmail.com>
2024-08-13 19:49:02 +0200

    chore: Update package.json and astro.config.mjs for server output and add auth-astro and @auth/core dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,15 @@ import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/serverless";
 import { defineConfig } from "astro/config";
 
+import auth from "auth-astro";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(), auth()],
   output: "server",
   adapter: vercel({
-    webAnalytics:{
-      enabled: true,
+    webAnalytics: {
+      enabled: true
     }
   })
 });
