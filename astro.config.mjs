5b2a4e9396143af0b132011c35d80b6c133260bd
Manu Gonzalez <manueljgonzalezg1698@gmail.com>
2024-08-14 18:02:26 +0200

    chore: Remove unused auth-astro package and update login page layout and functionality

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@ import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/serverless";
 import { defineConfig } from "astro/config";
 
-import auth from "auth-astro";
 
 // https://astro.build/config
 export default defineConfig({
