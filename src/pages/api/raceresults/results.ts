45cb8024c1dad2866c5cca86a994b5b57a3b1e84
Manu Gonzalez <manueljgonzalezg1698@gmail.com>
2024-08-17 18:49:50 +0200

    feat: Improve ResultsTable layout and functionality (with test file)

--- src/pages/api/raceresults/results.ts
+++ src/pages/api/raceresults/results.ts
@@ -1,126 +1,6 @@
-export interface Welcome {
-    Version:        number;
-    Cars:           Car[];
-    Events:         Event[];
-    Laps:           Lap[];
-    Result:         Result[];
-    Penalties:      null;
-    TrackConfig:    string;
-    TrackName:      string;
-    Type:           string;
-    Date:           Date;
-    SessionFile:    string;
-    SessionConfig:  SessionConfig;
-    ChampionshipID: string;
-    RaceWeekendID:  string;
-}
-
-export interface Car {
-    BallastKG:  number;
-    CarId:      number;
-    Driver:     Driver;
-    Model:      string;
-    Restrictor: number;
-    Skin:       string;
-    ClassID:    string;
-    MinPing:    number;
-    MaxPing:    number;
-}
-
-export interface Driver {
-    Guid:      string;
-    GuidsList: string[] | null;
-    Name:      string;
-    Nation:    string;
-    Team:      string;
-    ClassID:   string;
-}
-
-export interface Event {
-    CarId:           number;
-    Driver:          Driver;
-    ImpactSpeed:     number;
-    OtherCarId:      number;
-    OtherDriver:     Driver;
-    RelPosition:     Position;
-    Type:            Type;
-    WorldPosition:   Position;
-    Timestamp:       number;
-    AfterSessionEnd: boolean;
-}
-
-export interface Position {
-    X: number;
-    Y: number;
-    Z: number;
-}
-
-export enum Type {
-    CollisionWithCar = "COLLISION_WITH_CAR",
-    CollisionWithEnv = "COLLISION_WITH_ENV",
-}
-
-export interface Lap {
-    BallastKG:               number;
-    CarId:                   number;
-    CarModel:                string;
-    Cuts:                    number;
-    DriverGuid:              string;
-    DriverName:              string;
-    LapTime:                 number;
-    Restrictor:              number;
-    Sectors:                 number[];
-    Timestamp:               number;
-    Tyre:                    string;
-    ClassID:                 string;
-    ContributedToFastestLap: boolean;
-    SpeedTrapHits:           any[] | null;
-    Conditions:              Conditions;
-}
-
-export interface Conditions {
-    Ambient:       number;
-    Road:          number;
-    Grip:          number;
-    WindSpeed:     number;
-    WindDirection: number;
-    RainIntensity: number;
-    RainWetness:   number;
-    RainWater:     number;
-}
-
-export interface Result {
-    BallastKG:    number;
-    BestLap:      number;
-    CarId:        number;
-    CarModel:     string;
-    DriverGuid:   string;
-    DriverName:   string;
-    Restrictor:   number;
-    TotalTime:    number;
-    NumLaps:      number;
-    HasPenalty:   boolean;
-    PenaltyTime:  number;
-    LapPenalty:   number;
-    Disqualified: boolean;
-    ClassID:      string;
-    GridPosition: number;
-}
-
-export interface SessionConfig {
-    session_type:                         number;
-    name:                                 string;
-    time:                                 number;
-    laps:                                 number;
-    is_open:                              number;
-    wait_time:                            number;
-    visibility_mode:                      number;
-    qualifying_type:                      number;
-    qualifying_number_of_laps_to_average: number;
-    count_out_lap:                        boolean;
-    disable_push_to_pass:                 boolean;
-}
-
+import { cars } from "@/consts/cars";
+import { circuits } from "@/consts/circuits";
+import { circuitlayouts } from "@/consts/circuitlayouts";
 
 const btnCargaTabla = document.querySelector('#btnCargarTabla');
 if (btnCargaTabla) {
@@ -139,31 +19,40 @@ async function cargarJSON() {
         method: "GET",
     }).then(async (response) => {
         let data = await response.text();
-        console.log(data);
-        //console.log(this.responseText);
+        //console.log(data);
         let datos = JSON.parse(data);
-        //console.log(datos);
+        console.log(datos);
 
         let dcars = datos.Cars;
         let devents = datos.Events;
         let dlaps = datos.Laps;
         let dresult = datos.Result;
-        let dpenalties = datos.Penalties;
+
         const resultado = document.querySelector('#resultado');
         if (resultado) {
             resultado.innerHTML = '';
+        } else {
+            throw new Error('Element with id "resultado" not found.');
         }
+
+        const resultadoPista = document.querySelector('#resultado2');
+        if (resultadoPista) {
+            resultadoPista.innerHTML = '';
+        } else {
+            throw new Error('Element with id "resultado2" not found.');
+        }
+
         let pos = 0;
         for (let item of dresult) {
             //console.log(item.DriverName);
             pos = pos + 1;
             let gridPositionClass;
             if ((item.GridPosition - pos) > 0) {
-                gridPositionClass = "<i class='fa-solid fa-caret-up' style='color: #00ff04;'>";
+                gridPositionClass = '<svg viewBox="0 0 24 24" fill="#00f000" class="w-6 float mx-auto"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M11.375 6.22l-5 4a1 1 0 0 0 -.375 .78v6l.006 .112a1 1 0 0 0 1.619 .669l4.375 -3.501l4.375 3.5a1 1 0 0 0 1.625 -.78v-6a1 1 0 0 0 -.375 -.78l-5 -4a1 1 0 0 0 -1.25 0z" /></svg>';
             } else if ((item.GridPosition - pos) < 0) {
-                gridPositionClass = "<i class='fa-solid fa-caret-down' style='color: #ff0000;'>";
+                gridPositionClass = '<svg viewBox="0 0 24 24" fill="#ff0000" class="w-6 float mx-auto rotate-180"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M11.375 6.22l-5 4a1 1 0 0 0 -.375 .78v6l.006 .112a1 1 0 0 0 1.619 .669l4.375 -3.501l4.375 3.5a1 1 0 0 0 1.625 -.78v-6a1 1 0 0 0 -.375 -.78l-5 -4a1 1 0 0 0 -1.25 0z" /></svg>';
             } else if ((item.GridPosition - pos) === 0) {
-                gridPositionClass = "<i class='fa-solid fa-caret-right' style='color: #ffc800;'>";
+                gridPositionClass = '<svg viewBox="0 0 24 24" fill="#ffc800" class="w-6 float mx-auto rotate-90"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M11.375 6.22l-5 4a1 1 0 0 0 -.375 .78v6l.006 .112a1 1 0 0 0 1.619 .669l4.375 -3.501l4.375 3.5a1 1 0 0 0 1.625 -.78v-6a1 1 0 0 0 -.375 -.78l-5 -4a1 1 0 0 0 -1.25 0z" /></svg>';
             }
 
             // Obtener nombre de equipo + Ping Min-Max
@@ -172,27 +61,27 @@ async function cargarJSON() {
             for (let item2 of dcars) {
                 if (item2.CarId === carID) {
                     equipo = item2.Driver.Team;
-                    //console.log(item2.Driver.Team);
                     break;
                 }
+            }
+
+            // Obtener el nombre real del coche
+            const isCarExists = cars.find((car) => car.filename === item.CarModel);
+            let carName: string;
+            if(isCarExists){
+                carName = isCarExists.brand + " " + isCarExists.model;
+            } else{
+                carName = item.CarModel;
             }
 
             // Obtener tiempo total de carrera
             let timeadjust;
             if (item.Disqualified === false) {
                 timeadjust = (item.TotalTime / 1000) + (item.PenaltyTime / 1000000000);
-                let seconds = Number(Math.round(parseFloat(timeadjust % 60 + 'e' + 3)) + 'e-' + 3).toFixed(3).padStart(2, '0').slice(-2);
-                let minutes = Math.trunc(timeadjust / 60).toString().padStart(2, '0').slice(-2);
-                let hours = Math.trunc(timeadjust / 3600).toString().padStart(2, '0').slice(-2);
-                // if (Number(seconds) < 10) {
-                //     seconds.toLocaleString('es-ES', { minimumIntegerDigits: 2, useGrouping: false });
-                // }
-                // if (minutes < 10) {
-                //     minutes.toLocaleString('es-ES', { minimumIntegerDigits: 2, useGrouping: false });
-                // }
-                // if (hours < 10 && hours !== 0) {
-                //     hours.toLocaleString('es-ES', { minimumIntegerDigits: 2, useGrouping: false });
-                // }
+                let seconds = formatTwoIntegersPlusThreeDecimals(timeadjust % 60);
+                let minutes = formatTwoIntegers(Math.trunc((timeadjust / 60)%60));
+                let hours = formatTwoIntegers(Math.trunc(timeadjust / 3600));
+                //console.log("Pos: "+pos+" ->"+hours+":"+minutes+":"+seconds);
 
                 if (Number(hours) > 0) {
                     if (item.PenaltyTime !== 0) {
@@ -210,14 +99,14 @@ async function cargarJSON() {
             } else {
                 timeadjust = "DQ";
             }
-            //console.log("--" + pos + "--");
-            //console.log(item.DriverName + ": " + timeadjust);
 
             // Obtener numero de vueltas totales / vuelta rapida / neumatico
             let vueltastotales = 0;
             let bestlap = 0;
+            let bestlapToString: string;
             let tyre;
             let mediavueltas = 0;
+            let mediavueltasToString: string;
             let cuts = 0;
             for (let item3 of dlaps) {
                 if (item3.CarId === carID) {
@@ -234,30 +123,19 @@ async function cargarJSON() {
                 }
             }
 
-            // bestlap = (bestlap / 1000);
-            // let secondsbl = Number(Math.round(parseFloat(bestlap % 60 + 'e' + 3)) + 'e-' + 3).toFixed(3);
-            // let minutesbl = Math.trunc(bestlap / 60);
-            // if (secondsbl < 10) {
-            //     secondsbl = "0" + secondsbl;
-            // }
-            // if (minutesbl < 10) {
-            //     minutesbl = "0" + minutesbl;
-            // }
+            bestlap = (bestlap / 1000);
+            let secondsbl = formatTwoIntegersPlusThreeDecimals(bestlap % 60);
+            let minutesbl = formatTwoIntegers(Math.trunc((bestlap / 60)%60));
 
-            // bestlap = minutesbl + ":" + secondsbl;
-            // mediavueltas = mediavueltas / (vueltastotales - cuts);
-            // mediavueltas = (mediavueltas / 1000);
-            // let secondsmv = Number(Math.round(parseFloat(mediavueltas % 60 + 'e' + 3)) + 'e-' + 3).toFixed(3);
-            // let minutesmv = Math.trunc(mediavueltas / 60);
-            // if (secondsmv < 10) {
-            //     secondsmv = "0" + secondsmv;
-            // }
-            // if (minutesmv < 10) {
-            //     minutesmv = "0" + minutesmv;
-            // }
-            // mediavueltas = minutesmv + ":" + secondsmv;
-            // //console.log(mediavueltas+"/"+vueltastotales+"/"+cuts);
-            // //console.log(item.DriverName + ": " + vueltastotales);
+            bestlapToString = minutesbl.toString() + ":" + secondsbl.toString();
+            //bestlap = minutesbl.toString + ":" + secondsbl.toString;
+            mediavueltas = mediavueltas / (vueltastotales - cuts);
+            mediavueltas = (mediavueltas / 1000);
+            let secondsmv = formatTwoIntegersPlusThreeDecimals(mediavueltas % 60);
+            let minutesmv = formatTwoIntegers(Math.trunc((mediavueltas / 60)%60));
+            mediavueltasToString = minutesmv + ":" + secondsmv;
+            //console.log(mediavueltas+"/"+vueltastotales+"/"+cuts);
+            //console.log(item.DriverName + ": " + vueltastotales);
 
             let collisions = 0;
             for (let item4 of devents) {
@@ -267,41 +145,87 @@ async function cargarJSON() {
             }
             if (pos % 2 === 0) {
                 resultado.innerHTML += `
-                    <tr class="bg-[#0f0f0f]">
-                        <td>${pos}</td>                     <!-- Posicion -->
+                    <tr class="bg-[#0f0f0f] text-center">
+                        <td class = "font-medium">${pos.toString()}</td>                     <!-- Posicion -->
                         <td>${item.DriverName}</td>         <!-- Nombre -->
                         <td>${equipo}</td>                  <!-- Equipo -->
-                        <td>${item.CarModel}</td>           <!-- Coche -->
-                        <td>${item.GridPosition - pos} ${gridPositionClass}</i></td> <!-- Gan/Per -->
+                        <td>${carName}</td>           <!-- Coche -->
+                        <td>${gridPositionClass}</td>     <!-- Gan/Per (Flechas)-->
+                        <td class = "text-start">${Math.abs(item.GridPosition - pos)}</td> <!-- Gan/Per (Número)-->
                         <td>${timeadjust}</td>              <!-- Tiempo Total -->
                         <td>${vueltastotales}</td>          <!-- Nº Vueltas -->
-                        <td>${bestlap + " (" + tyre + ")"}</td> <!-- Vuelta Rapida  + Neumaticos-->
-                        <td>${mediavueltas}</td>            <!-- Media VRapida -->
+                        <td>${bestlapToString + " (" + tyre + ")"}</td> <!-- Vuelta Rapida  + Neumaticos-->
+                        <td>${mediavueltasToString}</td>            <!-- Media VRapida -->
                         <td>${collisions}</td>              <!-- Colisiones -->
                         <td>${item.BallastKG + " Kg / " + item.Restrictor + "%"}</td>  <!-- Ballast/Restrictor -->
                     </tr>
             `;
             } else {
                 resultado.innerHTML += `
-                    <tr class="bg-[#19191c]">
+                    <tr class="bg-[#19191c] text-center">
                         <td>${pos}</td>                     <!-- Posicion -->
                         <td>${item.DriverName}</td>         <!-- Nombre -->
                         <td>${equipo}</td>                  <!-- Equipo -->
-                        <td>${item.CarModel}</td>           <!-- Coche -->
-                        <td>${item.GridPosition - pos} ${gridPositionClass}</i></td> <!-- Gan/Per -->
+                        <td>${carName}</td>           <!-- Coche -->
+                        <td>${gridPositionClass}</td>     <!-- Gan/Per (Flechas)-->
+                        <td class = "text-start">${Math.abs(item.GridPosition - pos)}</td> <!-- Gan/Per (Número)-->
                         <td>${timeadjust}</td>              <!-- Tiempo Total -->
                         <td>${vueltastotales}</td>          <!-- Nº Vueltas -->
-                        <td>${bestlap + " (" + tyre + ")"}</td> <!-- Vuelta Rapida  + Neumaticos-->
-                        <td>${mediavueltas}</td>            <!-- Media VRapida -->
+                        <td>${bestlapToString + " (" + tyre + ")"}</td> <!-- Vuelta Rapida  + Neumaticos-->
+                        <td>${mediavueltasToString}</td>            <!-- Media VRapida -->
                         <td>${collisions}</td>              <!-- Colisiones -->
                         <td>${item.BallastKG + " Kg / " + item.Restrictor + "%"}</td>  <!-- Ballast/Restrictor -->
                     </tr>
             `;
             }
         }
+
+        // Pista y datos de la carrera
+
+        const isCircuitExists = circuits.find((circuit) => circuit.filename === datos.TrackName);
+        if(isCircuitExists){
+            const circuitName = isCircuitExists.name;
+            const circuitLocation = isCircuitExists.location;
+            if(datos.TrackConfig === null || datos.TrackConfig === undefined || datos.TrackConfig === ""){
+                datos.TrackConfig = "noname_trackconfig";
+            }
+            const layout = circuitlayouts.find((layout) => (layout.filename === datos.TrackConfig)&&(layout.circuit === isCircuitExists.id));
+            const layoutName = layout?.name;
+            const layoutLength = layout?.length;
+            const layoutCapacity = layout?.capacity;
+
+            resultadoPista.innerHTML += `
+                <div class="text-center bg-[#19191c] rounded-lg py-5" style = "width=99%">
+                <p class = "text-3xl font-bold border-b border-[#da392b] w-fit mx-auto mb-2">Datos del circuito</p>
+                    <div class = "grid grid-cols-1">
+                        <p class="text-2xl font-semibold">Circuito: ${circuitName} (Variante ${layoutName})</p>
+                        <p class="text-xl">Localización: ${circuitLocation}</p>
+                    </div>
+                    <div class = "grid grid-cols-2 text-lg mt-2">
+                        <p>Longitud: ${layoutLength} m</p>
+                        <p>Capacidad: ${layoutCapacity} pilotos</p>
+                    </div>
+                </div>
+                <p class="text-3xl font-bold border-b border-[#da392b] w-fit mx-auto mt-4 mb-2">Resultado de carrera</p>
+            `;
+        }
+
+
+
     }).catch((error) => {
         console.error(error);
     });
 }
 
+
+function formatTwoIntegersPlusThreeDecimals(num: number){
+    const integerPart = Math.floor(Math.abs(num)).toString().padStart(2, '0');
+    const decimalPart = Math.abs(num % 1).toFixed(3).slice(2);
+    const sign = num < 0 ? '-' : '';
+    return `${sign}${integerPart}.${decimalPart}`;
+}
+
+function formatTwoIntegers(num: number): string {
+    return Math.abs(num).toString().padStart(2, '0').slice(-2);
+}
 
