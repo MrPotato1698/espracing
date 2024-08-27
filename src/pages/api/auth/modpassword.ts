76c5b68539fef08303baf38b1b5e4a9a7503b11d
Manu Gonzalez <manueljgonzalezg1698@gmail.com>
2024-08-27 17:06:21 +0200

    feat: Add API route for modifying password (not working)

---
+++ src/pages/api/auth/modpassword.ts
@@ -0,0 +1,11 @@
+import type { APIRoute } from "astro";
+import { supabase } from "@/lib/supabase";
+
+export const POST: APIRoute = async ({ request, redirect }) => {
+  const formData = await request.formData();
+  const old_pass = formData.get("old_pass")?.toString();
+  const new_pass = formData.get("new_pass")?.toString();
+  const repeat_new_pass = formData.get("repeat_new_pass")?.toString();
+
+  return redirect("/");
+};
