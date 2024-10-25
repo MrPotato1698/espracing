329f6660c29533977b51e3b47917e51e5fd458aa
Manu Gonzalez <manueljgonzalezg1698@gmail.com>
2024-10-25 11:38:11 +0200

    Arreglo de obtención de datos de Base de Datos de Supabase

--- src/db/supabase.ts
+++ src/db/supabase.ts
@@ -1,10 +1,12 @@
 import { createClient } from "@supabase/supabase-js";
-import type { Database } from "@/types/supabase";
 
-export const supabase = createClient<Database>(
+export const supabase = createClient(
   import.meta.env.SUPABASE_URL,
   import.meta.env.SUPABASE_ANON_KEY,
   {
+    db:{
+      schema: 'public',
+    },
     auth: {
       autoRefreshToken: false,
       persistSession: false,
