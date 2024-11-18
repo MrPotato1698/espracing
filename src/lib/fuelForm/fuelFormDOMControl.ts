523fde396171bc8d113ada0316cfa62b253d5bd2
Manu Gonzalez <manueljgonzalezg1698@gmail.com>
2024-11-18 19:40:22 +0100

    Mejorar la lógica y lectura posterior de los resultados en el cálculo de combustible

--- src/lib/fuelForm/fuelFormDOMControl.ts
+++ src/lib/fuelForm/fuelFormDOMControl.ts
@@ -2,10 +2,10 @@ import type { Car } from "@/types/Car";
 import { cars } from "@/consts/cars";
 
 export function initializeFuelFormControls() {
-  const switchElement = document.getElementById("switch-2")as HTMLInputElement | null;
+  const switchElement = document.getElementById("switch-2") as HTMLInputElement | null;
   const timeContainer = document.getElementById("timeContainer")
   const lapsContainer = document.getElementById("lapsContainer");
-  const carSelect = document.getElementById("carname")as HTMLInputElement | null;
+  const carSelect = document.getElementById("carname") as HTMLInputElement | null;
   const otherCarFuelTank = document.getElementById("otherCarFuelTank");
   const formulario = document.getElementById('fuelForm') as HTMLFormElement | null;
   const resultadoDiv = document.getElementById('resultadoCalculo');
@@ -43,7 +43,7 @@ export function initializeFuelFormControls() {
         return;
       }
       fuelTank = parseFloat(fuelTankInput as string);
-    }else{
+    } else {
       const selectedCar = cars.find((car: Car) => car.filename === CarFileName);
       fuelTank = selectedCar ? selectedCar.maxLiter : 0;
     }
@@ -57,12 +57,12 @@ export function initializeFuelFormControls() {
     } else {
       const hours = parseInt(formData.get('racedurationTimeHours') as string);
       const minutes = parseInt(formData.get('racedurationTimeMinutes') as string);
-      distance = hours* 3600 + minutes*60;
+      distance = hours * 3600 + minutes * 60;
     }
 
     const laptimeMinutes = parseInt(formData.get('laptimeMinutes') as string);
     const laptimeSeconds = parseInt(formData.get('laptimeSeconds') as string);
-    const laptime = laptimeMinutes* 60 + laptimeSeconds;
+    const laptime = laptimeMinutes * 60 + laptimeSeconds;
 
     // Tratar los datos
     let VueltasTotales: number;
@@ -91,20 +91,34 @@ export function initializeFuelFormControls() {
 
     let stintLaps: number[] = [];
     let stintFuel: number[] = [];
-
+    let extraLap: boolean = false;
     if (minimumStints >= completeStints && minimumStints > 1) {
       for (let i = 0; i < completeStints; i++) {
-          const laps = Math.floor(maxLapsperTank);
-          stintLaps.push(laps);
-          const fuel = i === 0 ? fuelTank : (laps * fuelConsumption) - (fuelTank - stintFuel[i - 1]);
-          stintFuel.push(fuel);
+        const laps = Math.floor(maxLapsperTank);
+        stintLaps.push(laps);
+        const fuel = i === 0 ? fuelTank : (laps * fuelConsumption);
+        stintFuel.push(fuel);
       }
       // Ultimo Stint
-      const lastStintLaps = VueltasTotales - (completeStints) * Math.floor(maxLapsperTank);
-      const lastStintFuel = (lastStintLaps * fuelConsumption) - (100 - stintFuel[stintFuel.length - 1]);
-      stintLaps.push(lastStintLaps);
-      stintFuel.push(lastStintFuel);
-    }else if (minimumStints === 1) {
+      let lastStintLaps = VueltasTotales - (completeStints) * Math.floor(maxLapsperTank);
+      let lastStintFuel;
+      if (lastStintLaps > Math.floor(maxLapsperTank)) {
+        const laps = Math.floor(maxLapsperTank);
+        stintLaps.push(laps);
+        const fuel = (laps * fuelConsumption);
+        stintFuel.push(fuel);
+        lastStintLaps -= laps;
+        lastStintFuel = (lastStintLaps * fuelConsumption) - (fuelTank - stintFuel[stintFuel.length - 1]);
+        stintLaps.push(lastStintLaps);
+        stintFuel.push(lastStintFuel);
+        extraLap = true;
+      } else {
+        lastStintFuel = (lastStintLaps * fuelConsumption) - (fuelTank - stintFuel[stintFuel.length - 1]);
+        stintLaps.push(lastStintLaps);
+        stintFuel.push(lastStintFuel);
+        extraLap = false;
+      }
+    } else if (minimumStints === 1) {
       stintLaps.push(VueltasTotales);
       stintFuel.push(VueltasTotales * fuelConsumption);
     }
@@ -118,19 +132,27 @@ export function initializeFuelFormControls() {
       let resultHTML = `
       <h2 class="font-bold">Resultado</h2>
       <p class="font-semibold">Para completar la carrera necesitas:</p>
-      <ul>
-        <li>${stints.toFixed(2)} stints (${Math.floor(stints)} nº de paradas).</li>
-        <li>${totalFuel.toFixed(2)} litros de combustible para completar la carrera.</li>
-        <li>${fuelTank} litros de tanque de combustible.</li>
-      </ul>`;
+      <ul>`;
+      if (extraLap) {
+        resultHTML += `
+        <li><span class = 'font-medium text-[#da392b]'>${stints.toFixed(2)} stints</span> (${Math.floor(stints) + 1} paradas, si no se ahorra combustible lo que 
+        indica el ultimo stint).</li>`;
+      } else {
+        resultHTML += `
+        <li><span class = 'font-medium text-[#da392b]'>${stints.toFixed(2)} stints</span> (${Math.floor(stints)} paradas).</li>`;
+      }
       if (stints > 1) {
         resultHTML += `
+          <li><span class = 'font-medium text-[#da392b]'>${totalFuel.toFixed(2)} litros</span> de combustible para completar la carrera.</li>
+          <li><span class = 'font-medium text-[#da392b]'>${fuelTank} litros</span> de tanque de combustible.</li>
+          <li><span class = 'font-medium text-[#da392b]'>${maxLapsperTank.toFixed(0)} vueltas</span> completas por deposito.</li>
+        </ul>
         <p class = "mt-2 font-semibold">Detalle de los diferentes stints:</p>
         <ul>
           ${stintLaps.map((item, index) => {
-          return `<li>Stint ${index + 1}: ${item} vueltas, ${stintFuel[index].toFixed(2)} litros para realizar el stint,
-          ${totalFuelperStint[index].toFixed(2)} segundos solo la parada (sin contar paso por boxes).
-          - ${carFuelTimeperLiter} segundos por cada litro repostado.</li>`;
+          return `<li><span class = 'font-medium text-[#da392b]'>Stint ${index + 1}:</span> ${item} vueltas, ${stintFuel[index].toFixed(2)} litros 
+          para realizar el stint, ${totalFuelperStint[index].toFixed(2)} segundos solo la parada + tiempo de paso por boxes. (${carFuelTimeperLiter} s/litros repostado).
+          </li>`;
         }).join('')}
         </ul>
         `;
@@ -138,15 +160,15 @@ export function initializeFuelFormControls() {
         resultHTML += `
         <p class = "mt-2 font-semibold">Detalle del stint de carrera:</p>
         <ul>
-          <li>Unico stint: ${stintLaps[0]} vueltas, ${stintFuel[0].toFixed(2)} litros.
+          <li><span class = 'font-medium text-[#da392b]'>Unico stint:</span> ${stintLaps[0]} vueltas, ${stintFuel[0].toFixed(2)} litros.
           No es necesario parar, pero recuerda que cada litro añadido en la parada son ${carFuelTimeperLiter} segundos sin contar paso por boxes.</li>
         </ul>
         `;
       }
       if (carTiresTime > 0) {
         resultHTML += `
-        <p class = "mt-4">*Si se quiere realizar un cambio de gomas, recuerde que en este coche son ${carTiresTime} segundos,
-        mire los diferentes stints y si el tiempo de parada es superior al cambio de goma, recuerde que tiene un
+        <p class = "mt-4">*Si se quiere realizar un cambio de gomas, recuerde que en este coche son <span class = 'font-medium text-[#da392b]'>
+        ${carTiresTime} segundos</span>, mire los diferentes stints y si el tiempo de parada es superior al cambio de goma, recuerde que tiene un
         cambio de gomas "gratis" en lo que perdida de tiempo se refiere.</p>
       `;
       }
