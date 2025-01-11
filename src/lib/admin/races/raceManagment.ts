f4e75fb1132c9456ca5147ecd1b42501966f5248
Manu Gonzalez <manueljgonzalezg1698@gmail.com>
2025-01-11 19:04:21 +0100

    Agregar funcionalidad para editar carreras; implementar lógica de manejo de archivos JSON y actualización de datos en la base de datos.

--- src/lib/admin/races/raceManagment.ts
+++ src/lib/admin/races/raceManagment.ts
@@ -250,3 +250,138 @@ export function initDeleteRaceButtons() {
     });
   });
 }
+
+export function initEditRace() {
+
+  const form = document.getElementById('editRaceForm') as HTMLFormElement;
+
+  const switchS2Element = document.getElementById("switch-S2") as HTMLInputElement | null;
+  const switchR2Element = document.getElementById("switch-R2") as HTMLInputElement | null;
+
+  const fileInputS1R1 = document.getElementById("fileInputS1R1") as HTMLInputElement;
+  const fileInfoS1R1 = document.getElementById("fileInfoS1R1") as HTMLDivElement;
+  const fileNameS1R1 = document.getElementById("fileNameS1R1") as HTMLSpanElement;
+
+  const splitS2R1File = document.getElementById("split2R1File");
+  const fileInputS2R1 = document.getElementById("fileInputS2R1") as HTMLInputElement;
+  const fileInfoS2R1 = document.getElementById("fileInfoS2R1") as HTMLDivElement;
+  const fileNameS2R1 = document.getElementById("fileNameS2R1") as HTMLSpanElement;
+
+  const containerRace2 = document.getElementById("race2Files");
+  const fileInputS1R2 = document.getElementById("fileInputS1R2") as HTMLInputElement;
+  const fileInfoS1R2 = document.getElementById("fileInfoS1R2") as HTMLDivElement;
+  const fileNameS1R2 = document.getElementById("fileNameS1R2") as HTMLSpanElement;
+
+  const splitS2R2File = document.getElementById("split2R2File");
+  const fileInputS2R2 = document.getElementById("fileInputS2R2") as HTMLInputElement;
+  const fileInfoS2R2 = document.getElementById("fileInfoS2R2") as HTMLDivElement;
+  const fileNameS2R2 = document.getElementById("fileNameS2R2") as HTMLSpanElement;
+
+  function toggleInputsS2() {
+    if (splitS2R1File && splitS2R2File && switchS2Element) {
+      splitS2R1File.style.display = !switchS2Element.checked ? "none" : "block";
+      splitS2R2File.style.display = !switchS2Element.checked ? "none" : "block";
+    }
+  }
+
+  function toggleInputR2() {
+    if (containerRace2 && switchR2Element) {
+      containerRace2.style.display = !switchR2Element.checked ? "none" : "block";
+    }
+  }
+
+  switchS2Element ? switchS2Element.addEventListener("change", toggleInputsS2) : null;
+  switchR2Element ? switchR2Element.addEventListener("change", toggleInputR2) : null;
+
+  fileInputS1R1.addEventListener("change", () => {
+    const file = fileInputS1R1.files?.[0];
+    if (file) {
+      if (file.type === "application/json") {
+        fileNameS1R1.textContent = file.name;
+        fileInfoS1R1.classList.remove("hidden");
+      } else {
+        alert("Por favor, selecciona un archivo JSON válido.");
+        fileInputS1R1.value = ""; // Limpiar la selección
+        fileInfoS1R1.classList.add("hidden");
+      }
+    } else {
+      fileInfoS1R1.classList.add("hidden");
+    }
+  });
+
+  fileInputS2R1.addEventListener("change", () => {
+    const file = fileInputS2R1.files?.[0];
+    if (file) {
+      if (file.type === "application/json") {
+        fileNameS2R1.textContent = file.name;
+        fileInfoS2R1.classList.remove("hidden");
+      } else {
+        alert("Por favor, selecciona un archivo JSON válido.");
+        fileInputS2R1.value = ""; // Limpiar la selección
+        fileInfoS2R1.classList.add("hidden");
+      }
+    } else {
+      fileInfoS2R1.classList.add("hidden");
+    }
+  });
+
+  fileInputS1R2.addEventListener("change", () => {
+    const file = fileInputS1R2.files?.[0];
+    if (file) {
+      if (file.type === "application/json") {
+        fileNameS1R2.textContent = file.name;
+        fileInfoS1R2.classList.remove("hidden");
+      } else {
+        alert("Por favor, selecciona un archivo JSON válido.");
+        fileInputS1R2.value = ""; // Limpiar la selección
+        fileInfoS1R2.classList.add("hidden");
+      }
+    } else {
+      fileInfoS1R2.classList.add("hidden");
+    }
+  });
+
+  fileInputS2R2.addEventListener("change", () => {
+    const file = fileInputS2R2.files?.[0];
+    if (file) {
+      if (file.type === "application/json") {
+        fileNameS2R2.textContent = file.name;
+        fileInfoS2R2.classList.remove("hidden");
+      } else {
+        alert("Por favor, selecciona un archivo JSON válido.");
+        fileInputS2R2.value = ""; // Limpiar la selección
+        fileInfoS2R2.classList.add("hidden");
+      }
+    } else {
+      fileInfoS2R2.classList.add("hidden");
+    }
+  });
+
+  form.addEventListener('submit', async (e) => {
+    e.preventDefault();
+    const formData = new FormData(form);
+
+    try {
+      const response = await fetch('/api/admin/race/editrace', {
+        method: 'POST',
+        body: formData,
+      });
+
+      if (!response.ok) {
+        throw new Error('Error en la respuesta del servidor: ' + response.statusText);
+      }
+
+      const result = await response.json();
+
+      if (result.success) {
+        alert('Carrera actualizada con éxito');
+        window.location.href = '/admin/adminraces';
+      } else {
+        throw new Error(result.error || 'Error desconocido');
+      }
+    } catch (error) {
+      console.error('Error al actualizar la carrera:', error);
+      alert('Hubo un error al actualizar la carrera. Por favor, inténtalo de nuevo. Error: '+error);
+    }
+  });
+}
--- src/pages/api/admin/race/editrace.ts
+++ src/pages/api/admin/race/editrace.ts
@@ -1,59 +1,114 @@
 import type { APIRoute } from "astro";
 import { supabase } from "@/db/supabase";
-import { createRaceData } from "@/lib/results/resultConverter";
+import { createRaceData, createRaceDataMultipleSplits } from "@/lib/results/resultConverter";
 
 export const POST: APIRoute = async ({ request }) => {
   const formData = await request.formData();
+
+  console.log('FormData recibido:', Object.fromEntries(formData.entries()));
+
   const race_id = formData.get('race_id');
   const name = formData.get('name');
   const orderChamp = formData.get('orderChamp');
   const champID = formData.get('champID');
   const pointsystem = formData.get('pointsystem');
-  const fileInput = formData.get('fileInput') as File | null;
+  const switchS2Element = formData.get("switch-S2") as HTMLInputElement | null;
+  const switchR2Element = formData.get("switch-R2") as HTMLInputElement | null;
+  const fileInputS1R1 = formData.get("fileInputS1R1") as File | null;
+  const fileInputS2R1 = formData.get("fileInputS2R1") as File | null;
+  const fileInputS1R2 = formData.get("fileInputS1R2") as File | null;
+  const fileInputS2R2 = formData.get("fileInputS2R2") as File | null;
+
 
   if (!race_id) {
     return new Response(JSON.stringify({ error: 'ID de carrera no proporcionado' }), { status: 400 });
   }
 
   try {
-    let updateData: any = {};
-    if (name) updateData.name = name;
-    if (orderChamp) updateData.orderinchamp = Number(orderChamp);
-    if (champID) updateData.championship = Number(champID);
-    if (pointsystem) updateData.pointsystem = Number(pointsystem);
-
-    if (Object.keys(updateData).length > 0) {
-      const { error: updateError } = await supabase
-        .from('race')
-        .update(updateData)
-        .eq('id', race_id);
-
-      if (updateError) throw updateError;
-    }
-
-    if (fileInput && fileInput.size > 0) {
-      const content = await fileInput.text();
-      const json = JSON.parse(content);
-      const transformedJson = createRaceData(json);
-
-      const { data: raceData } = await supabase
-        .from('race')
-        .select('filename')
-        .eq('id', race_id)
-        .single();
-
-      if (raceData && raceData.filename) {
-        const { error: storageError } = await supabase.storage
-          .from('results')
-          .update(raceData.filename, JSON.stringify(transformedJson), {
-            contentType: 'application/json',
-          });
-
-        if (storageError) throw storageError;
+    if (fileInputS1R1 !== null) {
+      const fileS1R1 = fileInputS1R1;
+      let fileS2R1;
+      let fileS1R2;
+      let fileS2R2;
+      let numSplits: number = 1;
+      let transformedJsonR1: string = "{}";
+      let transformedJsonR2: string = "{}";
+      if (!fileS1R1) {
+        return new Response(JSON.stringify({ error: "Por favor, selecciona un archivo JSON para la Carrera 1 del Split 1." }), { status: 400 });
       }
-    }
 
-    return new Response(JSON.stringify({ success: true }), { status: 200 });
+      if (fileInputS1R2 !== null) {
+        if (fileInputS1R2 !== null) {
+          fileS1R2 = fileInputS1R2;
+          if (!fileS1R2) {
+            return new Response(JSON.stringify({ error: "Por favor, selecciona un archivo JSON para la Carrera 2 del Split 1." }), { status: 400 });
+          }
+        }
+        if (fileInputS2R1 !== null) {
+          fileS2R1 = fileInputS2R1;
+          if (!fileS2R1) {
+            return new Response(JSON.stringify({ error: "Por favor, selecciona un archivo JSON para la Carrera 1 del Split 2." }), { status: 400 });
+          }
+          if (fileInputS2R2 !== null) {
+            fileS2R2 = fileInputS2R2;
+            if (!fileS2R2) {
+              return new Response(JSON.stringify({ error: "Por favor, selecciona un archivo JSON para la Carrera 2 del Split 2." }), { status: 400 });
+            }
+          }
+          numSplits = 2;
+        }
+
+        const contentS1R1 = await fileS1R1.text();
+        const jsonS1R1 = JSON.parse(contentS1R1);
+        let jsonS1R2;
+        if (switchR2Element?.checked) {
+          const contentS1R2 = await fileS1R2?.text();
+          if (!contentS1R2) return new Response(JSON.stringify({ error: "Sin contenido en el archivo de Carrera 2 Split 1." }), { status: 400 });
+          jsonS1R2 = JSON.parse(contentS1R2);
+        }
+        if (switchS2Element?.checked) {
+          if (switchR2Element?.checked) {
+            const contentS2R2 = await fileS1R2?.text();
+            if (!contentS2R2) return new Response(JSON.stringify({ error: "Sin contenido en el archivo de Carrera 2 Split 2." }), { status: 400 });
+            const jsonS2R2 = JSON.parse(contentS2R2);
+            transformedJsonR2 = JSON.stringify(createRaceDataMultipleSplits(jsonS1R2, jsonS2R2));
+          }
+          const contentS2R1 = await fileS2R1?.text();
+          if (!contentS2R1) return new Response(JSON.stringify({ error: "Sin contenido en el archivo de Carrera 1 Split 2." }), { status: 400 });
+          const jsonS2R1 = JSON.parse(contentS2R1);
+          transformedJsonR1 = JSON.stringify(createRaceDataMultipleSplits(jsonS1R1, jsonS2R1));
+        } else {
+          transformedJsonR1 = JSON.stringify(createRaceData(jsonS1R1));
+          if (switchR2Element?.checked) {
+            transformedJsonR2 = JSON.stringify(createRaceData(jsonS1R2));
+          }
+        }
+      }
+
+      const updateData: any = {
+        ...(name && { name: name }),
+        ...(fileInputS1R1 && {filename: fileS1R1.name}),
+        ...(champID && { championship: champID }),
+        ...(orderChamp && { orderinchamp: orderChamp }),
+        ...(pointsystem && { pointsystem: pointsystem }),
+        ...(numSplits && { splits: numSplits }),
+        ...(fileInputS1R1 && {race_data_1: transformedJsonR1}),
+        ...(switchR2Element?.checked && {race_data_2: transformedJsonR2}),
+      };
+
+      if (Object.keys(updateData).length > 0) {
+        const { error: updateError } = await supabase
+          .from('race')
+          .update(updateData)
+          .eq('id', race_id);
+
+        if (updateError) throw updateError;
+      }
+
+      return new Response(JSON.stringify({ success: true }), { status: 200 });
+    } else {
+      return new Response(JSON.stringify({ error: 'No se ha proporcionado un archivo para la Carrera 1 del Split 1.' }), { status: 400 });
+    }
   } catch (error) {
     console.error('Error al actualizar la carrera:', error);
     return new Response(JSON.stringify({ error: 'Error al actualizar la carrera' }), { status: 500 });
