8469154c1bc0aac1e24fbcc9b4dd53e6b430c38a
Manu Gonzalez <manueljgonzalezg1698@gmail.com>
2025-01-11 13:04:32 +0100

    Agregar campo para número total de carreras y estado de finalización en la gestión de campeonatos; actualizar lógica de envío y manejo de datos en el formulario.

--- src/pages/api/admin/championship/editchamp.ts
+++ src/pages/api/admin/championship/editchamp.ts
@@ -1,6 +1,5 @@
 import type { APIRoute } from "astro";
 import { supabase } from "@/db/supabase";
-import { createRaceData } from "@/lib/results/resultConverter";
 
 export const POST: APIRoute = async ({ request }) => {
   const formData = await request.formData();
@@ -9,7 +8,9 @@ export const POST: APIRoute = async ({ request }) => {
   const keySearchAPI = formData.get('keySearchAPI');
   const yearChamp = formData.get('yearChamp');
   const season = formData.get('season') as string;
-  const champORevent = formData.get('champORevent')  === 'on';
+  const champORevent = formData.get('champORevent')  !== null;
+  const numberTotalRaces = formData.get('numbertotalraces');
+  const isfinished = formData.get('isFinished') !== null;
 
   const seasonPart1 = '20' + season.slice(0, 2);
       const seasonPart2 = '20' + season.slice(2, 4);
@@ -21,23 +22,22 @@ export const POST: APIRoute = async ({ request }) => {
   }
 
   try {
-    let updateData: any = {};
-    if (champ_name) updateData.name = champ_name;
-    if (keySearchAPI) updateData.key_search = keySearchAPI;
-    if (yearChamp) updateData.year = Number(yearChamp);
-    if (season) updateData.season = formattedSeason;
-    if(champORevent) updateData.ischampionship = champORevent;
+    const updateData: any = {
+      ...(champ_name && { name: champ_name }),
+      ...(keySearchAPI && { key_search: keySearchAPI }),
+      ...(yearChamp && { year: Number(yearChamp) }),
+      ...(season && { season: formattedSeason }),
+      ischampionship: champORevent,
+      ...(numberTotalRaces && { number_of_races_total: Number(numberTotalRaces) }),
+      isfinished: isfinished
+    };
 
-    console.log(updateData);
+    const { error: updateError } = await supabase
+      .from('championship')
+      .update(updateData)
+      .eq('id', champ_id);
 
-    if (Object.keys(updateData).length > 0) {
-      const { error: updateError } = await supabase
-        .from('championship')
-        .update(updateData)
-        .eq('id', champ_id);
-
-      if (updateError) throw updateError;
-    }
+    if (updateError) throw updateError;
 
     return new Response(JSON.stringify({ success: true }), { status: 200 });
   } catch (error) {
