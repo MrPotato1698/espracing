528568a03224877b2b91263ed063d4e2585cc37b
Manu Gonzalez <manueljgonzalezg1698@gmail.com>
2025-01-13 17:27:23 +0100

    Creación de librería para alojar interfaces genéricas. Agregar interfaces para la gestión de datos de la tabla de resultados, automóviles y circuitos.

---
+++ src/types/Utils.ts
@@ -0,0 +1,37 @@
+export interface ResultTableData {
+    gridPositionClass: string;
+    gainsAbs: string;
+    posicionFinal: string;
+    driverName: string;
+    carColorClass: string;
+    carClass: string;
+    carBrand: string;
+    carName: string;
+    team: string;
+    totalLaps: string;
+    timeadjust: string;
+    gap: string;
+    interval: string;
+    flapClass: string;
+    bestlapToString: string;
+    tyre: string;
+    points: string;
+    splitNumber: number;
+}
+
+export interface CarData{
+    filename: string;
+    brand: string;
+    model: string;
+    classShortName: string;
+    classColor: string;
+    imgbrand: string;
+}
+
+export interface CircuitData{
+  name: string;
+  layout: string;
+  location: string;
+  length: number;
+  capacity: number;
+}
