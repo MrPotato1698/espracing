2840650c78dc6b43dc9c64d1bb387b9d11f3dd79
Manu Gonzalez <manueljgonzalezg1698@gmail.com>
2025-01-25 19:59:09 +0100

    Eliminar archivos de configuración de Cloudinary y componentes de modal de imagen para simplificar el código y mejorar la mantenibilidad.

--- src/lib/cloudinary.ts
+++
@@ -1,8 +0,0 @@
-const cloudinary = require('cloudinary');
-
-cloudinary.v2.config({
-  cloud_name: import.meta.env.PUBLIC_CLOUDINARY_CLOUD_NAME,
-  api_key: import.meta.env.PUBLIC_CLOUDINARY_API_KEY,
-  api_secret: import.meta.env.CLOUDINARY_API_SECRET,
-  secure: true,
-});
