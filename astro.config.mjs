b78e6dd7e88821e2a7f6e9d55f9aa593c6d82442
Manu Gonzalez <manueljgonzalezg1698@gmail.com>
2025-01-30 21:04:47 +0100

    Integrar Tailwind V4 en la configuración de Astro y eliminar el archivo de configuración de Tailwind V3.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,22 @@
-import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel";
 import { defineConfig } from "astro/config";
 
 
+import tailwindcss from "@tailwindcss/vite";
+
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [],
   output: "server",
+
   adapter: vercel({
     webAnalytics: {
       enabled: true
     }
   }),
-  
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
