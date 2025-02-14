1b548be92f77642c54e27a4fa8b84c96ca7f145d
Manu Gonzalez <manueljgonzalezg1698@gmail.com>
2025-02-14 14:12:42 +0100

    Agregar comentarios sobre la configuración del proxy del servidor para pruebas en desarrollo.

--- astro.config.mjs
+++ astro.config.mjs
@@ -19,4 +19,15 @@ export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
+
+  // Server proxy local, para pruebas en desarrollo
+  // server: {
+  //   proxy: {
+  //     '/stracker': {
+  //       target: 'https://es2.assettohosting.com:10018',
+  //       changeOrigin: true,
+  //       secure: false,
+  //     },
+  //   },
+  // },
 });
