35a805eb2fc093b251c38d6b5142e7850ee4009e
Manu Gonzalez <manueljgonzalezg1698@gmail.com>
2025-02-24 18:58:09 +0100

    Mejorar la gestión de invitaciones a equipos, incluyendo la aceptación y rechazo de solicitudes, y agregar el campo 'type' para distinguir entre unirse o crear un equipo.

--- src/pages/api/teams/acceptTeamInvitation.ts
+++ src/pages/api/teams/acceptTeamInvitation.ts
@@ -10,30 +10,67 @@ export const POST: APIRoute = async ({ request }) => {
 
   const { data: ReadedData } = await supabase
     .from('teamsapplication')
-    .select('user_application, team_requesting')
+    .select('*')
     .eq('id', id)
     .single();
 
   if (ReadedData) {
-    const { data: updateData, error: errorUpdateData } = await supabase
-      .from('profiles')
-      .update({ team: ReadedData.team_requesting })
-      .eq('id', ReadedData.user_application);
+    if(ReadedData.type === "join"){
+      const { data: updateData, error: errorUpdateData } = await supabase
+        .from('profiles')
+        .update({ team: ReadedData.team_requesting })
+        .eq('id', ReadedData.user_application);
 
-    if (errorUpdateData) {
-      return new Response("Error al actualizar el usuario", { status: 400 });
+      if (errorUpdateData) return new Response("Error al actualizar el usuario", { status: 400 });
+
+      const { error: deleteData } = await supabase
+        .from('teamsapplication')
+        .delete()
+        .eq('id', id);
+
+      if(deleteData) return new Response("Error al eliminar la solicitud final después de actualizar los datos", { status: 400 });
+
+    } else {
+      const { error: errorAcceptNewTeam } = await supabase
+      .from('team')
+      .update({ active: true })
+      .eq('id', ReadedData.team_requesting);
+
+      if (errorAcceptNewTeam) return new Response("Error al aceptar el equipo", { status: 400 });
+
+      const {error: errorNewTeamManager } = await supabase
+        .from('profiles')
+        .update({ is_team_manager: true, team: ReadedData.team_requesting })
+        .eq('id', ReadedData.user_application);
+
+      if (errorNewTeamManager) return new Response("Error al actualizar el usuario", { status: 400 });
+
+      const { data: UserRole } = await supabase
+        .from ('profiles')
+        .select('roleesp')
+        .eq('id', ReadedData.user_application)
+        .single();
+
+      if(UserRole?.roleesp && UserRole?.roleesp === 1){
+        const {error: errorChangeRole } = await supabase
+          .from('profiles')
+          .update({ roleesp: 2 })
+          .eq('id', ReadedData.user_application);
+
+        if (errorChangeRole) return new Response("Error al actualizar el usuario en su rol", { status: 400 });
+      }
+
+      const { error: deleteData } = await supabase
+        .from('teamsapplication')
+        .delete()
+        .eq('id', id);
+
+      if(deleteData) return new Response("Error al eliminar la solicitud final después de actualizar los datos", { status: 400 });
     }
 
-    const { data: deleteData } = await supabase
-      .from('teamsapplication')
-      .delete()
-      .eq('id', id);
-
+    return new Response("Petición aceptada con éxito", { status: 200 });
   } else {
-    console.log("ID de usuario no encontrado");
-    return new Response("ID de usuario no encontrado", { status: 400 });
+    console.log("ID de solicitud no encontrado");
+    return new Response("ID de solicitud no encontrado", { status: 400 });
   }
-
-
-  return new Response("Petición aceptada con éxito", { status: 200 });
 };
--- src/pages/api/teams/rejectTeamInvitation.ts
+++ src/pages/api/teams/rejectTeamInvitation.ts
@@ -4,25 +4,27 @@ import {supabase} from "@/db/supabase";
 export const POST: APIRoute = async ({ request }) => {
 
   const { action, id } = await request.json();
-  if(!id) {
-    return new Response("Id is required", { status: 400 });
-  }
+  if(!id) return new Response("Id is required", { status: 400 });
 
   const {data: ReadedData} = await supabase
     .from('teamsapplication')
-    .select('user_application, team_requesting')
+    .select('*')
     .eq('id', id)
     .single();
 
-    if (ReadedData?.user_application) {
-      const {data: deleteData} = await supabase
+    if (ReadedData) {
+      if(ReadedData.type === "create") {
+        const {error: deleteTeam} = await supabase.from('team').delete().eq('id', ReadedData.team_requesting);
+        if(deleteTeam) return new Response("Error al eliminar el equipo", { status: 400 });
+      }
+
+      const {error: deleteData} = await supabase
         .from('teamsapplication')
         .delete()
         .eq('id', id);
-    } else {
-      return new Response("ID de usuario no encontrado", { status: 400 });
-    }
 
+      if(deleteData) return new Response("Error al eliminar la solicitud", { status: 400 });
 
-  return new Response("Petición rechazada con éxito", { status: 200 });
+      return new Response("Petición rechazada con éxito", { status: 200 });
+    } else return new Response("ID de usuario no encontrado", { status: 400 });
 };
