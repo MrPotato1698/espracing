7a067222bb4d854e170d42bdcb305cb2a2a7ca38
Manu Gonzalez <manueljgonzalezg1698@gmail.com>
2025-06-29 19:11:25 +0200

    Agregar funciones para abrir y cerrar inscripciones en la API, añadiendo Cron Jobs de Vercel sustituyendo los Cron Job de Supabase

---
+++ src/pages/api/admin/inscriptions/close.ts
@@ -0,0 +1,38 @@
+import type { APIRoute } from "astro";
+import { supabase } from "@/db/supabase";
+
+export const GET: APIRoute = async ({ request }) => {
+  try{
+    const { data, error } = await supabase.functions.invoke('close-inscriptions', {body: { name: 'Functions' },});
+    if (error) {
+      return new Response(
+        JSON.stringify({
+          error: true,
+          message: error.message ?? "Unknown error",
+          details: error.details ?? null,
+        }),
+        {
+          status: 400,
+          headers: { "Content-Type": "application/json" },
+        }
+      );
+    }
+    return new Response(JSON.stringify({ success: true, data }), {
+      status: 200,
+      headers: { "Content-Type": "application/json" },
+    });
+  } catch (error: any) {
+    return new Response(
+      JSON.stringify({
+        error: true,
+        message: "Error al cerrar inscripciones",
+        details: error?.message ?? String(error),
+        stack: error?.stack ?? null,
+      }),
+      {
+        status: 500,
+        headers: { "Content-Type": "application/json" },
+      }
+    );
+  }
+};
